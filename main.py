10803e60a186e11abac8cbffde134f74467a3c6f
NexTry <www.nextry@gmail.com>
2022-08-02 14:14:14 +0700

    ok

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print("Hello World!")
+import discord
+import configparser
 
-x = 5
+bot = discord.Bot()
+
+
+def main():
+    bot.run(pars('token'))
+
+
+def pars(arg: str):
+    try:
+        parser = configparser.ConfigParser()
+        parser.read('config.ini')
+        result = parser["Discord"][arg]
+        return result
+    except:
+        return 0
+
+
+if __name__ == '__main__':
+    main()
