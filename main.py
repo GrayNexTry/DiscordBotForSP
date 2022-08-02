c42dc5ec79561867bf34343dfdd54e23df295b57
NexTry <www.nextry@gmail.com>
2022-08-02 14:55:15 +0700

    Discord bot

--- main.py
+++ main.py
@@ -7,15 +7,6 @@ bot = discord.Bot()
 
 def main():
     bot.run(pars('token'))
-    connect = sqlite3.connect('SPData.db')
-    cursor = connect.cursor()
-
-    cursor.execute("SELECT * FROM ShopData")
-    print(cursor.fetchall())
-
-    cursor.close()
-
-
 
 def pars(arg: str):
     try:
